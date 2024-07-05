5eddb215dc63e0297a57671551657894d945d984
thearhansheth <arhan.sheth@gmail.com>
2024-07-05 11:18:12 +0530

    Modified number of epochs to increase learning capabilities

--- yoloDetector.py
+++ yoloDetector.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8s.yaml")
 
 # training model
 results = model.train(data = "/Users/arhan.sheth/Documents/Codes/DX/Detection/objectDetection/configs.yaml", 
-                      epochs = 30,
+                      epochs = 200,
                       learning_rate = 0.001,
                       augmentation = True)
 
