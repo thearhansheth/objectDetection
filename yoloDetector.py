5bd86cfc1bd6fba32219cbc6295677ac8bdb95a2
thearhansheth <arhan.sheth@gmail.com>
2024-07-05 11:16:55 +0530

    YOLO version changed and training data updated

--- yoloDetector.py
+++ yoloDetector.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 
 # load a model
-model = YOLO("yolov8n.yaml")
+model = YOLO("yolov8s.yaml")
 
 # training model
-results = model.train(data = "/Users/arhan.sheth/Documents/Codes/DX/Detection/objectDetection/configs.yaml", epochs = 10)
+results = model.train(data = "/Users/arhan.sheth/Documents/Codes/DX/Detection/objectDetection/configs.yaml", 
+                      epochs = 30,
+                      learning_rate = 0.001,
+                      augmentation = True)
+
